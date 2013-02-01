2cfdb385791950c4ea1b4190f5b408a91726a969
Frolle <frollix@razzan.net>
2013-02-01 09:42:14 +0100

    Programmed the controller

--- src/main/java/com/frollz/service/StudentService.java
+++ src/main/java/com/frollz/service/StudentService.java
@@ -1,5 +1,13 @@
 package com.frollz.service;
 
-public interface StudentService {
+import java.util.List;
 
+import com.frollz.model.Student;
+
+public interface StudentService {
+		public void add(Student student);
+		public void edit(Student student);
+		public void delete(int studentID);
+		public Student getStudent(int studentID);
+		public List getAllStudents();
 }
